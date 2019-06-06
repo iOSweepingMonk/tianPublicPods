62f7d96c8c70d92e9e9036d22b47ef492a72e52f
iOSweepingMonk <740305122@qq.com>
2019-06-06 14:45:55 +0800

    init library

---
+++ Example/Pods/Target Support Files/ColorTool1/ColorTool1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ColorTool1 : NSObject
+@end
+@implementation PodsDummy_ColorTool1
+@end
---
+++ Example/Pods/Target Support Files/Pods-ColorTool1_Tests/Pods-ColorTool1_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ColorTool1_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ColorTool1_Tests
+@end
